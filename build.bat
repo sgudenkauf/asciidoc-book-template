0ccfe870c2b9b5ed9eab2fb8c0a0bfaf1bbeae58
sgudenkauf <51904739+sgudenkauf@users.noreply.github.com>
2024-10-24 16:14:24 +0200

    Improved file structure for different types of documents.

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
 
-start /b asciidoctor       src\asciidoc-book-master.adoc -o out\asciidoc-book-master.html
-start /b asciidoctor-pdf   src\asciidoc-book-master.adoc -o out\asciidoc-book-master.pdf
-start /b asciidoctor-epub3 src\asciidoc-book-master.adoc -o out\asciidoc-book-master.epub
-start /b .asciidoctor-web-pdf\asciidoctor-web-pdf.exe src\asciidoc-book-master.adoc -o out\asciidoc-book-master-WEB.pdf
+start /b asciidoctor       src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.html
+start /b asciidoctor-pdf   src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.pdf
+start /b asciidoctor-epub3 src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.epub
+start /b .asciidoctor-web-pdf\asciidoctor-web-pdf.exe src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master-WEB.pdf
 REM start /b .asciidoctor-web-pdf\asciidoctor-web-pdf.exe src\asciidoc-book-master.adoc -a stylesheet="+..\css\myStyle.css" -o out\asciidoc-book-master-WEB.pdf
 
-EXIT
+EXIT 
