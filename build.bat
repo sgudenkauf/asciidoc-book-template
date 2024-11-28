a537d28b1f0e3adc3496a475097d64462ea2b59e
sgudenkauf <51904739+sgudenkauf@users.noreply.github.com>
2024-11-28 16:33:03 +0100

    Added gitignore

--- build.bat
+++ build.bat
@@ -1,9 +1,12 @@
 @echo off
 
-start /b asciidoctor       src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.html
-start /b asciidoctor-pdf   src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.pdf
-start /b asciidoctor-epub3 src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.epub
-start /b .asciidoctor-web-pdf\asciidoctor-web-pdf.exe src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master-WEB.pdf
-REM start /b .asciidoctor-web-pdf\asciidoctor-web-pdf.exe src\asciidoc-book-master.adoc -a stylesheet="+..\css\myStyle.css" -o out\asciidoc-book-master-WEB.pdf
+REM start /b asciidoctor       src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.html
+REM start /b asciidoctor-pdf   src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.pdf
+REM start /b asciidoctor-epub3 src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master.epub
+
+REM start /b .asciidoctor-web-pdf\asciidoctor-web-pdf.exe src\book\asciidoc-book-master.adoc -o out\book\asciidoc-book-master-WEB.pdf
+ REM start /b .asciidoctor-web-pdf\asciidoctor-web-pdf.exe src\asciidoc-book-master.adoc -a stylesheet="+..\css\myStyle.css" -o out\asciidoc-book-master-WEB.pdf
+
+start /b .asciidoctor-reveal-js\asciidoctor-revealjs-win.exe src\slides\slides.adoc -o out\slides\slides.pdf
 
 EXIT 
